1f3b0bdbe7194ec7f8507e1ba9405ee403cc4228
GitHub Action <action@github.com>
2020-08-18 06:52:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/forezp/atlasplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/forezp/atlasplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.forezp.atlasplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.forezp.atlasplugin.MyBundle
 
 class MyApplicationService {
 
